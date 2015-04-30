2e9a14affd2c0630e46191707b1c5937b86c0380
karan2209 <kkhiara@tavisca.com>
2015-04-30 17:23:24 +0530

    first commit

---
+++ TryingOutStuff/Program.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace TryingOutStuff
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<Car> cars = GetCarList();
+//            //IEnumerable<Car> recentCars = cars.Where(car => car.Year > 1989);
+//            List<Car> recentCars = cars.Where(car => car.Year > 1989).ToList();
+//            List<string> carMakes = cars.Select(car => car.Make).ToList();
+
+//            foreach (var carMake in carMakes)
+//            {
+//                Console.WriteLine(carMake);
+//            }
+
+//        }
+
+//        public static List<Car> GetCarList()
+//        {
+//            List<Car> cars = new List<Car>()
+//                {
+//                    new Car() {Make = "Ferrari",Year = 1989},
+//                    new Car() {Make = "Ford",Year = 2005},
+//                    new Car() {Make = "General Motors",Year = 1997},
+//                    new Car() {Make = "Toyota",Year = 1989},
+//                };
+//            return cars;
+//        }
+
+//        public static void GetCarListWithOwner()
+//        {
+
+
+//        }
+//    }
+
+//    public class Car
+//    {
+//        public string Make { get; set; }
+//        public int Year { get; set; }
+//    }
+
+//    public class Owner
+//    {
+//        public string Name { get; set; }
+//    }
+//}
---
+++ TryingOutStuff/RestGetCountryInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryingOutStuff
+{
+    class RestGetCountryInfo
+    {
+        static void Main(string[] args)
+        {
+            HttpResponseMessage httpResponseMessage = GetCountryInfoByCode("IN");
+            Console.WriteLine(httpResponseMessage.ToString());
+        }
+
+        public static HttpResponseMessage GetCountryInfoByCode(string code)
+        {
+            HttpClient httpClient = new HttpClient();
+            Uri uri = new Uri("https://restcountries.p.mashape.com/alpha/" + code);
+            httpClient.DefaultRequestHeaders.Add("X-Mashape-Key", "sSBP29CDyYmsh0q0kRjSkfArmGfQp1gL9RujsniegbbYSXn8rm");
+            HttpResponseMessage httpResponseMessage = httpClient.GetAsync(uri).Result;
+            return httpResponseMessage;
+        }
+    }
+}
